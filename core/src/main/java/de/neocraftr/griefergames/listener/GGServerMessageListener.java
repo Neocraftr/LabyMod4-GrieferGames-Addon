6a47cd6cb5e8e1aa05240c613e959c6086a37e7e
Neocraftr <51837582+Neocraftr@users.noreply.github.com>
2023-07-14 01:17:20 +0200

    Translate MysteryMod subtitles

--- core/src/main/java/de/neocraftr/griefergames/listener/GGServerMessageListener.java
+++ core/src/main/java/de/neocraftr/griefergames/listener/GGServerMessageListener.java
@@ -2,11 +2,19 @@ package de.neocraftr.griefergames.listener;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import de.neocraftr.griefergames.GrieferGames;
+import net.labymod.api.Laby;
+import net.labymod.api.client.component.Component;
+import net.labymod.api.client.component.serializer.legacy.LegacyComponentSerializer;
 import net.labymod.api.event.Subscribe;
 import net.labymod.api.event.client.network.server.NetworkPayloadEvent;
 import net.labymod.api.event.client.network.server.NetworkPayloadEvent.Side;
+import net.labymod.core.main.LabyMod;
+import net.labymod.core.main.serverapi.model.LabyModSubtitle;
+import net.labymod.serverapi.protocol.model.display.Subtitle;
 import net.labymod.serverapi.protocol.payload.io.PayloadReader;
+import java.util.UUID;
 
 public class GGServerMessageListener {
   private final GrieferGames griefergames;
@@ -46,6 +54,16 @@ public class GGServerMessageListener {
       if(messageKey.equals("redstone")) {
         String redstoneState = message.getAsJsonObject().get("status").getAsString();
         griefergames.setRedstoneActive(redstoneState.equals("0"));
+      }
+
+      if(messageKey.equals("user_subtitle")) {
+        JsonObject subtitleData = message.getAsJsonArray().get(0).getAsJsonObject();
+
+        String text = Laby.labyAPI().minecraft().componentMapper().translateColorCodes(subtitleData.get("text").getAsString());
+        Component component = LegacyComponentSerializer.legacySection().deserialize(text);
+
+        Subtitle subtitle = new LabyModSubtitle(UUID.fromString(subtitleData.get("targetId").getAsString()), 1.2, component);
+        LabyMod.references().subtitleService().addSubtitle(subtitle);
       }
     }
   }
