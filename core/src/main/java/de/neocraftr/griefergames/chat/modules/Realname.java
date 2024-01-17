5bd1e6f264ce86ad03e4a63380876ec9c9d7655a
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2024-01-17 16:34:24 +0100

    add realname-message-control for the CLOUD

--- core/src/main/java/de/neocraftr/griefergames/chat/modules/Realname.java
+++ core/src/main/java/de/neocraftr/griefergames/chat/modules/Realname.java
@@ -12,6 +12,7 @@ public class Realname extends ChatModule {
   private final GrieferGames griefergames;
   private final Pattern realnameRegex = Pattern.compile("^(?:\\[[^\\]]+\\])?[A-Za-z\\-]+\\+? \\u2503 \\u007E?\\!?\\w{1,16} ist (\\!?\\w{1,16})$");
 
+  private final Pattern realnameRegexCloud = Pattern.compile("^(?:\\[[^\\]]+\\])?\\[GrieferGames\\] (?:The real name of|Der echte Name von) \\w{1,16} (?:is|ist) (\\!?\\w{1,16}).$");
   public Realname(GrieferGames griefergames) {
     this.griefergames = griefergames;
   }
@@ -19,13 +20,13 @@ public class Realname extends ChatModule {
   @Subscribe
   public void messageProcessEvent(GGChatProcessEvent event) {
     if(event.isCancelled()) return;
-    if(!griefergames.isSubServerType(SubServerType.REGULAR)) return;
     RealnamePosition position = griefergames.configuration().chatConfig().getRealnamePosition();
     if (position == RealnamePosition.DEFAULT) return;
     if (event.getMessage().getPlainText().isBlank()) return;
 
     Matcher matcher = realnameRegex.matcher(event.getMessage().getPlainText());
-    if (matcher.find()) {
+    Matcher matcherCloud = realnameRegexCloud.matcher(event.getMessage().getPlainText());
+    if (matcher.find() || matcherCloud.find()) {
       if (position == RealnamePosition.SECONDCHAT) {
         event.setSecondChat(true);
       } else if (position == RealnamePosition.BOTH) {
